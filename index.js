14e31cdd3a0261b82c4b96bc02d9b0df903a832e
fabriciocunhauk <fabriciocunhadeveloper@gmail.com>
2025-02-17 18:24:20 +0000

    Removed unused body-parser

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
-import parseReceiptRoute from './routes/ParseReceipt.js'; // Import the router
-import priceListRoute from './routes/PriceList.js'; // Import the router
+import parseReceiptRoute from './routes/ParseReceipt.js'; 
+import priceListRoute from './routes/PriceList.js'; 
 
 const app = express();
 const port = 3333;
@@ -10,9 +10,8 @@ const port = 3333;
 app.use(cors());
 app.use(helmet());
 
-
-app.use('/api/parse-receipt', parseReceiptRoute); // Use the imported router
-app.use('/api/price-list', priceListRoute); // Use a distinct path for priceListRoute
+app.use('/api/parse-receipt', parseReceiptRoute); 
+app.use('/api/price-list', priceListRoute); 
 
 app.listen({ host: '0.0.0.0', port: process.env.PORT ?? 3333 }, () => {
   console.log(`Server started on http://localhost:${port}`);
